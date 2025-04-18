777d787ad94586f40065db3b88a630a12cbd1f9b
JoseVS1 <105169283+JoseVS1@users.noreply.github.com>
2025-04-17 20:31:18 -0600

    Style 404 page

--- src/pages/NoMatch.jsx
+++ src/pages/NoMatch.jsx
@@ -1,7 +1,7 @@
 export const NoMatch = () => {
   return (
-    <div>
-        <h1>404 Page not found</h1>
+    <div className="not-found-page">
+        <h1><span>404</span> Page not found :(</h1>
     </div>
   )
 }
